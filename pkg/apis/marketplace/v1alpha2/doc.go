e75e68ea15b072253fbefd75e1715e48d6e4b97f
maxpaspa <maxpaspa@ibm.com>
2020-06-04 14:48:10 -0400

    group name change in doc.go

--- pkg/apis/marketplace/v1alpha2/doc.go
+++ pkg/apis/marketplace/v1alpha2/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha2 contains API Schema definitions for the razee v1alpha2 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=razee.marketplace.redhat.com
+// +groupName=marketplace.redhat.com
 package v1alpha2
